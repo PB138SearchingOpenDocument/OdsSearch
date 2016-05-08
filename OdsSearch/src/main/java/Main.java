24c0473f14e79c308ef82435d2d78dd80990f6dd
marek urban <umarekk@gmail.com>
2016-05-08 23:46:30 +0200

    Zmena ODF kniznice, vyclenenie logiky do triedy OdsSearch upravena funkcia search() pre vyhladavanie aj ciastocnej zhody, nova trieda pre zaznamy vysledkov QueryItem, upravena funkcia main na citanie z konzoly s volenim vyhladavanych stringov a zmenou vstupneho subora.

--- OdsSearch/src/main/java/Main.java
+++ OdsSearch/src/main/java/Main.java
@@ -1,61 +1,43 @@
-import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
-import org.odftoolkit.odfdom.doc.table.OdfTable;
-import org.odftoolkit.odfdom.doc.table.OdfTableCell;
-
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /**
- * @author Marek Abaffy (422572)
+ * @author Marek Abaffy (422572), Marek Urban (422252)
  */
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Configuring ODS module for default input file [products_small.ods] ...");
 
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Two arguments (file path, searched word) are required.");
-        }
+        String filePath = new String("products_small.ods");
+        OdsSearch ods = new OdsSearch();
+        ods.setup(filePath);
 
         try {
-            OdfSpreadsheetDocument ods = OdfSpreadsheetDocument.loadDocument(args[0]);
-            List<OdfTable> tableList = ods.getTableList();
-            for (OdfTable odfTable : tableList) {
-                OdfTableCell result = searchInTable(odfTable, args[1]);
-                if (result != null) {
-                    System.out.println(String.format("Sheet [%s] Position [%d,%d]", odfTable.getTableName(), result.getColumnIndex(), result.getRowIndex()));
+            BufferedReader br = new BufferedReader(new
+                    InputStreamReader(System.in));
+
+            String userInput;
+            String filePathConsole;
+
+            System.out.println("Module is ready.");
+            do {
+                System.out.println("Enter search keyword or \n[!] - Exit\n[+] - Change input file");
+                userInput = (String) br.readLine();
+                if(userInput.equals("+")){
+                    System.out.println("Enter new file path:");
+                    filePathConsole = (String) br.readLine();
+                    ods.setup(filePathConsole);
                 }
-            }
+                if(!userInput.equals("+") && !userInput.equals("!")){
+                    ods.search(userInput);
+                    ods.printResults();
+                    ods.clearResults();
+                }
+            } while(!userInput.equals("!"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Method searches expression in given table.
-     *
-     * @param table      Table to search expression in.
-     * @param expression Expression to be search.
-     * @return TableCell containing search expression.
-     */
-    private static OdfTableCell searchInTable(OdfTable table, String expression) {
-        for (int i = 0; i < table.getColumnCount(); i++) {
-            OdfTableCell cell = table.getCellByPosition(i, 0);
-
-            if (cell.getStringValue().isEmpty()) {
-                return null;
-            }
-
-            for (int j = 0; j < table.getRowList().size(); j++) {
-
-                cell = table.getCellByPosition(i, j);
-                if (cell.getStringValue().isEmpty()) {
-                    break;
-                }
-
-                if (table.getCellByPosition(i, j).getStringValue().equals(expression)) {
-                    return table.getCellByPosition(i, j);
-                }
-            }
-        }
-        return null;
     }
 }
